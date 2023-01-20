3c901bd944a32096f39f90f26aba937ce2a4520b
whejy <john.wheldale@outlook.com>
2023-01-20 14:34:14 +1030

    10.3 add repositories list

---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+import Constants from 'expo-constants';
+import RepositoryList from './RepositoryList';
+import { Text, StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Rate Repository Application</Text>
+      <RepositoryList />
+    </View>
+  );
+};
+
+export default Main;
